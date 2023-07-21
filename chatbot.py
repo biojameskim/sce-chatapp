163b3227069b2e00a2b59e93ebaee962c47877d6
James Kim <biojameskim2002@gmail.com>
2023-07-21 10:07:59 -0700

    Configure correct env variable setup

--- chatbot.py
+++ chatbot.py
@@ -14,10 +14,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-os.environ['OPENAI_API_TYPE'] = os.environ.get('OPENAI_API_TYPE')
-os.environ['OPENAI_API_VERSION'] = os.environ.get('OPENAI_API_VERSION')
-os.environ['OPENAI_API_BASE'] = os.environ.get('OPENAI_API_BASE')
-os.environ['OPENAI_API_KEY'] = os.environ.get('OPENAI_API_KEY')
+OPENAI_API_TYPE = os.environ.get('OPENAI_API_TYPE')
+OPENAI_API_VERSION = os.environ.get('OPENAI_API_VERSION')
+OPENAI_API_BASE = os.environ.get('OPENAI_API_BASE')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 # Langchain imports
 from langchain.vectorstores import FAISS
